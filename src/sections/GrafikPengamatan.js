5f16312810b71533674345d13efc8dd84e4c2f91
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-04-30 21:59:09 +0700

    initial commit graph

---
+++ src/sections/GrafikPengamatan.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// import Tabel from '../components/Tabel';
+// import Grafik from '../components/Grafik';
+
+export default class GrafikPengamatan extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Grafik Pengamatan</h1>
+      </div>
+    );
+  }
+}
