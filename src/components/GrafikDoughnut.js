c794c004bb6b34ea9d13b040d7f7cb331b168945
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-05-02 15:18:39 +0700

    fully functional props

--- src/components/GrafikDoughnut.js
+++ src/components/GrafikDoughnut.js
@@ -1,7 +1,44 @@
 import React, { Component } from 'react';
+import { Doughnut } from 'react-chartjs-2';
 
 export default class GrafikDoughnut extends Component {
   render() {
-    return <div />;
+    const data = {
+      labels: this.props.labelArea,
+      datasets: [
+        {
+          data: this.props.dataArea,
+          backgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#273c75',
+            '#e84118',
+            '#dcdde1',
+            '#fbc531',
+            '#353b48',
+            '#7f8fa6',
+            '#c23616'
+          ],
+          hoverBackgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#273c75',
+            '#e84118',
+            '#dcdde1',
+            '#fbc531',
+            '#353b48',
+            '#7f8fa6',
+            '#c23616'
+          ]
+        }
+      ]
+    };
+    return (
+      <div>
+        <Doughnut data={data} />
+      </div>
+    );
   }
 }
