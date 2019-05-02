573061d21c675ebdf706dbd5a1f43bd576f29c4d
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-05-02 16:37:48 +0700

    merged

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import "./style/App.css";
+import React, { Component } from 'react';
+import './style/App.css';
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Intro from "./sections/Intro";
-import DataPengamatan from "./sections/DataPengamatan";
-import GrafikPengamatan from "./sections/GrafikPengamatan";
-import DataHasil from "./sections/DataHasil";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Intro from './sections/Intro';
+import DataPengamatan from './sections/DataPengamatan';
+import GrafikPengamatan from './sections/GrafikPengamatan';
+import DataHasil from './sections/DataHasil';
 
 export default class App extends Component {
   render() {
@@ -15,8 +15,8 @@ export default class App extends Component {
         <Navbar />
         <Intro />
         <DataPengamatan />
-        <DataHasil />
         <GrafikPengamatan />
+        <DataHasil />
         <Footer />
       </div>
     );
