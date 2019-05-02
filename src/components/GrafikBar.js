9ccf0bba8101b0a416f1088ebeb048a30a5949e2
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-05-02 17:56:29 +0700

    bar graph using props !

--- src/components/GrafikBar.js
+++ src/components/GrafikBar.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
+import { Bar } from 'react-chartjs-2';
 
 export default class GrafikBar extends Component {
   render() {
-    return <div />;
+    const data = {
+      labels: this.props.labelArea,
+      datasets: [
+        {
+          label: this.props.labelGraph,
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: this.props.dataArea
+        }
+      ]
+    };
+    return (
+      <div>
+        <h2>{this.props.judul}</h2>
+        <Bar
+          data={data}
+          width={100}
+          height={50}
+          options={{
+            maintainAspectRatio: false
+          }}
+        />
+      </div>
+    );
   }
 }
