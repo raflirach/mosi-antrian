c194abc0a6efa719056e3f71f600ad58f3bf6481
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-05-02 17:57:46 +0700

    give some data to graph using props

--- src/sections/GrafikPengamatan.js
+++ src/sections/GrafikPengamatan.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import GrafikDoughnut from '../components/GrafikDoughnut';
+import GrafikBar from '../components/GrafikBar';
+
 let dataArea = require('../data/dataArea.json');
+let dataTabelSimulasi = require('../data/dataTabelSimulasi.json');
 
 export default class GrafikPengamatan extends Component {
   render() {
@@ -10,12 +13,26 @@ export default class GrafikPengamatan extends Component {
     let dataAreaProps = dataArea.map(data => {
       return data.durasi;
     });
+    let labelTabel = dataTabelSimulasi.map(data => {
+      return data.i;
+    });
+    let dataTabel = dataTabelSimulasi.map(data => {
+      return data.totalWaktu;
+    });
+
     return (
       <div id="section3" className="container">
         <h1>Grafik Pengamatan</h1>
         <br />
-        <h3>Grafik Data Pengamatan</h3>
-        <GrafikDoughnut labelArea={labelArea} dataArea={dataAreaProps} />
+        <br />
+        <GrafikDoughnut judul={'Grafik Frekuensi Tujuan Antar'} labelArea={labelArea} dataArea={dataAreaProps} />
+        <br />
+        <GrafikBar
+          judul={'Grafik Total Waktu Pelayanan'}
+          labelGraph={'Total Waktu Pelayanan dalam Menit'}
+          labelArea={labelTabel}
+          dataArea={dataTabel}
+        />
       </div>
     );
   }
