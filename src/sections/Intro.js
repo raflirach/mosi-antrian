3d163681571ce51da9dc9dc685d090a7e018ede8
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-05-02 12:53:47 +0700

    adding tempat

--- src/sections/Intro.js
+++ src/sections/Intro.js
@@ -10,8 +10,8 @@ export default class Intro extends Component {
           <h1 className="header center orange-text">Pemodelan dan Simulasi - 3</h1>
           <div className="row center">
             <h5 className="header col s12 light">
-              Studi Kasus antrian diambil dari Tempat Pencucian Motor X. Dta pengamatan dilakukan dalam dua hari diambil
-              di hari Jumat 19 April 2019 dan Sabtu 20 April 2019 pada segmen waktu 13.00-15.00.
+              Studi Kasus antrian diambil dari Tempat Pencucian Motor BaronWash. Dta pengamatan dilakukan dalam dua hari
+              diambil di hari Jumat 19 April 2019 dan Sabtu 20 April 2019 pada segmen waktu 13.00-15.00.
             </h5>
           </div>
           <div className="row center">
